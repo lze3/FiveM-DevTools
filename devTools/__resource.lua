dc6aa594575899ad9c7de5cd4aec9b95cb02d0bc
Jack Hodgson <hodgsonjack@hotmail.com>
2019-05-11 17:59:43 +0100

    ADD: use wildcards instead of manually adding file names.

--- devTools/__resource.lua
+++ devTools/__resource.lua
@@ -1,11 +1,11 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 server_scripts {
-    "player_s.lua" -- a player server script
+    "*_s.lua" 
 }
 
 client_scripts {
-    "vehicle_c.lua" -- a vehicle client script
+    "*_c.lua" 
 }
 
 --[[
